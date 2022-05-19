abec2ebbdff14377a0ce6cdb756012e215ab5de9
DiegoRCarvalho <diegoribeirodecarvalho@gmail.com>
2022-05-19 11:43:29 -0300

    feat: add create user userCase

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -9,8 +9,16 @@ interface IRequest {
 class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
+  execute({ name, email }: IRequest): User {
+    const alreadyExistsEmail = this.usersRepository.findByEmail(email);
+
+    if (alreadyExistsEmail) {
+      throw new Error("Email is already being used.");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
